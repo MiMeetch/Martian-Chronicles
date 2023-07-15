db9bf852f52f46757df4de4e0dfff7cc0259d2b0
Mitch Hollyfield <hollyfieldmitch@gmail.com>
2023-07-15 14:30:21 -0400

    centered entry button and fixed background

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const image = document.getElementById('image')
+window.addEventListener('DOMContentLoaded', function() {
+    const backgroundElement = document.getElementById('background');
     fetch(`https://api.nasa.gov/planetary/apod?api_key=Ebu12BEIOtx4Dz9PPjuHwb6IrwUchNylWAqw9pYS`)
     .then((resp) => resp.json())
     .then(function(data) {
         console.log(data)
-        const hdurl = data.hdurl;
-        image.src = hdurl;
+        const backgroundImageUrl = data.hdurl;
+        backgroundElement.style.backgroundImage = 'url(' + backgroundImageUrl + ')';
     })
 })

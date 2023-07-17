ad507354e51574f148543ff3cbf566804d60c906
Mitch Hollyfield <hollyfieldmitch@gmail.com>
2023-07-16 23:58:17 -0400

    Updated Button Text & added additional API variables for APOD

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const backgroundElement = document.getElementById('background');
     fetch(`https://api.nasa.gov/planetary/apod?api_key=Ebu12BEIOtx4Dz9PPjuHwb6IrwUchNylWAqw9pYS`)
     .then((resp) => resp.json())
     .then(function(data) {
         console.log(data)
+        const backgroundElement = document.getElementById('background');
         const backgroundImageUrl = data.hdurl;
+        const backgroundImageTitle = data.title;
+        const backgroundImageExplanation = data.explanation;
         backgroundElement.style.backgroundImage = 'url(' + backgroundImageUrl + ')';
+        console.log(backgroundImageTitle)
+        console.log(backgroundImageExplanation)
     })
 })

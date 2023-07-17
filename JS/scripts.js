4263cc2f9ae8a4b2438382bb10964a7c0cb01e35
Mitch Hollyfield <hollyfieldmitch@gmail.com>
2023-07-17 00:17:49 -0400

    Added APOD API Title Data to splash page, updated text to monospace

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,10 +4,12 @@ window.addEventListener('DOMContentLoaded', function() {
     .then(function(data) {
         console.log(data)
         const backgroundElement = document.getElementById('background');
+        const backgroundTitle = document.getElementById('APOD-Title')
         const backgroundImageUrl = data.hdurl;
         const backgroundImageTitle = data.title;
         const backgroundImageExplanation = data.explanation;
         backgroundElement.style.backgroundImage = 'url(' + backgroundImageUrl + ')';
+        backgroundTitle.innerText = backgroundImageTitle;
         console.log(backgroundImageTitle)
         console.log(backgroundImageExplanation)
     })

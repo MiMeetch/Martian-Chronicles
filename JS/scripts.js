ebb3b43ac09210c664ecb0aa585d226283bb541e
Mitch Hollyfield <hollyfieldmitch@gmail.com>
2023-07-17 00:54:38 -0400

    Updated HTML title and implemented APOD title typing feature

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,15 +2,26 @@ window.addEventListener('DOMContentLoaded', function() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=Ebu12BEIOtx4Dz9PPjuHwb6IrwUchNylWAqw9pYS`)
     .then((resp) => resp.json())
     .then(function(data) {
+
         console.log(data)
+
         const backgroundElement = document.getElementById('background');
         const backgroundTitle = document.getElementById('APOD-Title')
+
         const backgroundImageUrl = data.hdurl;
         const backgroundImageTitle = data.title;
-        const backgroundImageExplanation = data.explanation;
+
         backgroundElement.style.backgroundImage = 'url(' + backgroundImageUrl + ')';
-        backgroundTitle.innerText = backgroundImageTitle;
-        console.log(backgroundImageTitle)
-        console.log(backgroundImageExplanation)
+
+        let i = 0;
+
+        const intervalId = setInterval(function() {
+            backgroundTitle.innerHTML += backgroundImageTitle[i]
+            i++;
+            if(i===backgroundImageTitle.length) {
+                clearInterval(intervalId)
+                return;
+            }
+        }, 100)
     })
 })

9c73715f5b75e19ad2a59276967c1b622246cf52
Mitch Hollyfield <hollyfieldmitch@gmail.com>
2023-07-18 21:16:27 -0400

    realigned goal starting point

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=Ebu12BEIOtx4Dz9PPjuHwb6IrwUchNylWAqw9pYS`)
     .then((resp) => resp.json())
     .then(function(data) {
-
+        
         console.log(data);
 
         const backgroundElement = document.getElementById('background');
